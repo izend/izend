c484afb51b2327011c06f19dafafee23219f1f36
frasq <frasq@izend.org>
2011-07-31 22:10:48 +0200

    Can search a list of threads in page action

--- actions/page.php
+++ actions/page.php
@@ -3,14 +3,17 @@
 /**
  *
  * @copyright  2010-2011 izend.org
- * @version    2
+ * @version    3
  * @link       http://www.izend.org
  */
+
+require_once 'models/thread.inc';
 
 function page($lang, $arglist=false) {
 	global $default_folder;
 
 	$folder=$page=false;
+	$folder_id=$page_id=false;
 
 	if (is_array($arglist)) {
 		if (isset($arglist[1])) {
@@ -21,6 +24,24 @@ function page($lang, $arglist=false) {
 			$folder=$default_folder;
 			$page=$arglist[0];
 		}
+	}
+
+	if (!$folder or !$page) {
+		return run('error/notfound', $lang);
+	}
+
+	foreach (is_array($folder) ? $folder : array($folder) as $folder) {
+		$folder_id = thread_id($folder);
+		if ($folder_id) {
+			$page_id = thread_node_id($folder_id, $page);
+			if ($page_id) {
+				break;
+			}
+		}
+	}
+
+	if (!$folder_id or !$page_id) {
+		return run('error/notfound', $lang);
 	}
 
 	require_once 'actions/folderpage.php';
