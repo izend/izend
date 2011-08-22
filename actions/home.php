cc9b0a7499e0a45f6e47005bd2b622ef8ecd590b
frasq <frasq@izend.org>
2011-08-22 16:25:46 +0200

    Recovers home action

--- actions/home.php
+++ actions/home.php
@@ -2,35 +2,43 @@
 
 /**
  *
- * @copyright  2011 frasq.org
+ * @copyright  2010-2011 izend.org
  * @version    2
- * @link       http://www.frasq.org
+ * @link       http://www.izend.org
  */
 
-function home($lang) {
-	head('title', translate('home:title', $lang));
+require_once 'userhasrole.php';
+require_once 'models/node.inc';
 
-	$search_text='';
-	$search_url=url('search', $lang);
-	$suggest_url=url('suggest', $lang);
-	$search=view('searchinput', $lang, compact('search_url', 'search_text', 'suggest_url'));
-	$sidebar = view('sidebar', false, compact('search'));
+function home($lang) {
+	global $root_node;
+
+	$r = node_get($lang, $root_node);
+	if (!$r) {
+		return run('error/notfound', $lang);
+	}
+	extract($r); /* node_name node_title node_abstract node_cloud node_created node_modified */
+
+	head('title', translate('home:title', $lang));
+	if ($node_abstract) {
+		head('description', $node_abstract);
+	}
+	if ($node_cloud) {
+		head('keywords', $node_cloud);
+	}
 
 	$languages='home';
 	$contact=$account=true;
-
-	$search=compact('search_url', 'search_text', 'suggest_url');
-	$banner = build('banner', $lang, compact('languages', 'contact', 'account', 'search'));
+	$edit=user_has_role('writer') ? url('editpage', $_SESSION['user']['locale']) . '/'. $root_node . '?' . 'clang=' . $lang : false;
+	$validate=url('home', $lang);
+	$banner = build('banner', $lang, compact('languages', 'contact', 'account', 'edit', 'validate'));
 
 	$contact_page=url('contact', $lang);
 	$footer = view('footer', $lang, compact('contact_page'));
 
-	$homeblog = build('homeblog', $lang);
-	$social = view('social', $lang);
+	$content = build('nodecontent', $lang, $root_node);
 
-	$content = view('home', $lang, compact('homeblog', 'social'));
-
-	$output = layout('standard', compact('footer', 'banner', 'content', 'sidebar'));
+	$output = layout('standard', compact('footer', 'banner', 'content'));
 
 	return $output;
 }
