06d56fede6240b668706e85ce670d7b41f1661a6
frasq <frasq@izend.org>
2011-08-31 12:22:29 +0200

    Adds function validate_website to library

---
+++ library/validatewebsite.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ *
+ * @copyright  2010-2011 izend.org
+ * @version    1
+ * @link       http://www.izend.org
+ */
+
+function validate_website($url) {
+	return preg_match('#^(http(s)?://)?(www\.)?([^\.]+)\.[a-z]{2,}$#', $url);
+}
+
