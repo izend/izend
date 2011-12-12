3e1168550a1594a5418b9457f00a95ffa0c9a03d
frasq <frasq@izend.org>
2011-12-12 18:19:35 +0100

    Corrects file_mime_type for host systems with a file command which only supports -i

--- library/filemimetype.php
+++ library/filemimetype.php
@@ -3,25 +3,24 @@
 /**
  *
  * @copyright  2010-2011 izend.org
- * @version    3
+ * @version    4
  * @link       http://www.izend.org
  */
 
 function file_mime_type($file, $encoding=true) {
 	$file = escapeshellarg($file);
-	$arg = $encoding ? '--mime-type --mime-encoding' : '--mime-type';
-	$cmd = "file $arg $file";
+	$cmd = "file -iL $file";
 
 	exec($cmd, $output, $r);
 
 	if ($r == 0) {
 		$s = $output[0];
-		$pos = strpos($s, ': ');
-		if ($pos) {
-			return substr($s, $pos+2);	/* keep charset? */
+		$beg = strpos($s, ': ');
+		if ($beg) {
+			$end = $encoding ? false : strpos($s, '; ');
+			return $end ? substr($s, $beg+2, $end - ($beg + 2)) : substr($s, $beg+2);
 		}
 	}
 
 	return 'application/octet-stream';
 }
-
