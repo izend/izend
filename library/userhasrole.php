0420179725382e49fcf332eeeaddb11e51a75499
frasq <frasq@izend.org>
2011-12-16 17:53:00 +0100

    Checks if user's role is empty in user_has_role

--- library/userhasrole.php
+++ library/userhasrole.php
@@ -3,13 +3,13 @@
 /**
  *
  * @copyright  2010-2011 izend.org
- * @version    2
+ * @version    3
  * @link       http://www.izend.org
  */
 
 require_once 'userisidentified.php';
 
 function user_has_role($role) {
-	return user_is_identified() and in_array($role, $_SESSION['user']['role']);
+	return user_is_identified() and !empty($_SESSION['user']['role']) and in_array($role, $_SESSION['user']['role']);
 }
 
