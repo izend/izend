922b0961747393c54a3e6746e1ba91d5ff7de7e4
frasq <frasq@izend.org>
2012-02-19 18:06:10 +0100

    Corrects validate_website

--- library/validatewebsite.php
+++ library/validatewebsite.php
@@ -3,12 +3,12 @@
 /**
  *
  * @copyright  2010-2011 izend.org
- * @version    2
+ * @version    3
  * @link       http://www.izend.org
  */
 
 function validate_website($url) {
-	return preg_match('#^(http(s)?://)?(www\.)?([^\.]+)\.[a-z]{2,}$#', $url);
+	return preg_match('#^(http(s)?://)?(www\.)?(([^\.]+)\.)+[a-z]{2,}$#', $url);
 }
 
 function normalize_website($url) {
