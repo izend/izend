03faf686a864bc7e8f1827ee76f3acf7a38a9a93
frasq <frasq@izend.org>
2012-02-28 20:01:01 +0100

    Puts $base_path, $base_url and $base_root in context in seval

--- library/seval.php
+++ library/seval.php
@@ -2,12 +2,14 @@
 
 /**
  *
- * @copyright  2010-2011 izend.org
- * @version    1
+ * @copyright  2010-2012 izend.org
+ * @version    2
  * @link       http://www.izend.org
  */
 
 function seval($s) {
+	global $base_path, $base_url, $base_root;
+
 	ob_start();
 	echo eval('?>'. $s);
 	return ob_get_clean();
