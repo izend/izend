bb7924f1c883644f3dd94b08b0ac8e980f34f788
frasq <frasq@izend.org>
2012-03-28 17:19:32 +0200

    Adds validate_color to library

---
+++ library/validatecolor.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ *
+ * @copyright  2010-2012 izend.org
+ * @version    1
+ * @link       http://www.izend.org
+ */
+
+function validate_color($color, $with_hashmark=true) {
+	return preg_match($with_hashmark ? '/^#?([0-9A-F]){6}$/' : '/^([0-9A-F]){6}$/', $color);
+}
+
