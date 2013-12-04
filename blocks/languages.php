1868222e844593a6e0308ca345d504650b06dc97
izend <izend@izend.org>
2013-12-04 13:36:25 +0100

    Adds German, Italian, Spanish and Russian flags and Paypal buttons

--- blocks/languages.php
+++ blocks/languages.php
@@ -2,8 +2,8 @@
 
 /**
  *
- * @copyright  2010-2011 izend.org
- * @version    3
+ * @copyright  2010-2013 izend.org
+ * @version    4
  * @link       http://www.izend.org
  */
 
@@ -16,8 +16,12 @@ function languages($lang, $action, $arg=false) {
 
 	$fr_page=($lang != 'fr' && in_array('fr', $supported_languages)) ? url($action, 'fr', $arg) : false;
 	$en_page=($lang != 'en' && in_array('en', $supported_languages)) ? url($action, 'en', $arg) : false;
+	$de_page=($lang != 'de' && in_array('de', $supported_languages)) ? url($action, 'de', $arg) : false;
+	$it_page=($lang != 'it' && in_array('it', $supported_languages)) ? url($action, 'it', $arg) : false;
+	$es_page=($lang != 'es' && in_array('es', $supported_languages)) ? url($action, 'es', $arg) : false;
+	$ru_page=($lang != 'ru' && in_array('ru', $supported_languages)) ? url($action, 'ru', $arg) : false;
 
-	$output = view('languages', false, compact('fr_page', 'en_page'));
+	$output = view('languages', false, compact('fr_page', 'en_page', 'de_page', 'it_page', 'es_page', 'ru_page'));
 
 	return $output;
 }
