6350c181d6d4cffe6e066ad2765eaeb17266ba2b
izend <izend@izend.org>
2014-07-19 17:27:12 +0200

    Tricks thread URL when searching $newsletter_thread or $rss_thread in searchlist.php

--- blocks/searchlist.php
+++ blocks/searchlist.php
@@ -2,13 +2,13 @@
 
 /**
  *
- * @copyright  2010-2012 izend.org
- * @version    7
+ * @copyright  2010-2014 izend.org
+ * @version    8
  * @link       http://www.izend.org
  */
 
 function searchlist($lang, $rsearch, $taglist) {
-	global $default_folder, $newsletter_thread;
+	global $default_folder, $newsletter_thread, $rss_thread;
 	global $base_path;
 
 	$linklist=array();
@@ -23,16 +23,22 @@ function searchlist($lang, $rsearch, $taglist) {
 			$link_cloud[]=in_array($w, $taglist) ? "<span class=\"tag\">$tag</span>" : $tag;
 		}
 		$link_cloud=implode(' ', $link_cloud);
+
 		if ($thread_type == 'folder' and ((is_array($default_folder) and in_array($thread_id, $default_folder)) or $thread_id == $default_folder)) {
 			$thread_url=$base_path . '/' . $lang;
 		}
 		else if ($thread_type == 'newsletter' and $thread_id == $newsletter_thread) {
-			$thread_url=url($thread_type, $lang);
+			$thread_url=url('newsletter', $lang);
+		}
+		else if ($thread_type == 'rss' and $thread_id == $rss_thread) {
+			$thread_url=url('rssfeed', $lang);
 		}
 		else {
 			$thread_url=url($thread_type, $lang) . '/'. $thread_name;
 		}
+
 		$link_url=$thread_url . '/' . $node_name;
+
 		if (!isset($linklist[$thread_id])) {
 			$content=array(compact('link_title', 'link_url', 'link_description', 'link_cloud'));
 			$linklist[$thread_id]=compact('thread_title', 'content');
