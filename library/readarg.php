013e29fa90b27a2f109e4cf4e188c79ff1b7b482
izend <izend@izend.org>
2014-12-11 19:02:56 +0100

    Restores processing an array argument in readarg

--- library/readarg.php
+++ library/readarg.php
@@ -3,11 +3,15 @@
 /**
  *
  * @copyright  2010-2014 izend.org
- * @version    4
+ * @version    5
  * @link       http://www.izend.org
  */
 
 function readarg($s, $trim=true, $strip=true) {
+	if (is_array($s)) {
+		return array_map('readarg', $s, array_fill(0, count($s), $trim), array_fill(0, count($s), $strip));
+	}
+
 	if (get_magic_quotes_gpc()) {
 		$s = stripslashes($s);
 	}
