bbc93aa7b49f5b5b89860e9d05a7c27800d8278e
izend <izend@izend.org>
2014-12-15 15:25:10 +0100

    Corrects function client_ip_address

--- library/clientipaddress.php
+++ library/clientipaddress.php
@@ -3,11 +3,11 @@
 /**
  *
  * @copyright  2010-2014 izend.org
- * @version    4
+ * @version    5
  * @link       http://www.izend.org
  */
 
 function client_ip_address() {
-	return isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : php_sapi_name() == 'cli' ? '127.0.0.1' : false;	// DON'T TRUST $_SERVER['HTTP_X_FORWARDED_FOR']
+	return isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : (php_sapi_name() == 'cli' ? '127.0.0.1' : false);	// DON'T TRUST $_SERVER['HTTP_X_FORWARDED_FOR']
 }
 
