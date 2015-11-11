2c51fd8644461bdb43cb932c2d2375a8f9305c97
izend <izend@izend.org>
2015-11-12 00:07:15 +0100

    Replaces create_function with function in function ucnatwords

--- library/ucnatwords.php
+++ library/ucnatwords.php
@@ -2,8 +2,8 @@
 
 /**
  *
- * @copyright  2010-2011 izend.org
- * @version    1
+ * @copyright  2010-2015 izend.org
+ * @version    2
  * @link       http://www.izend.org
  */
 
@@ -11,6 +11,6 @@ require_once 'strflat.php';
 
 function ucnatwords($s) {
 	// capitalize first letter in every word after removing accents
-	return preg_replace_callback("#(^|[ '~-])(\w+)#", create_function('$e', 'return $e[1].strtoupper($e[2][0]).substr($e[2], 1);'), strtolower(strflat($s)));
+	return preg_replace_callback("#(^|[ '~-])(\w+)#", function($e) { return $e[1].strtoupper($e[2][0]).substr($e[2], 1); }, strtolower(strflat($s)));
 }
 
