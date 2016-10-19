867f95b42a15b90ba4561e01b16ececf4cc18542
izend <izend@izend.org>
2016-10-20 00:32:46 +0200

    Makes parameter $arglist optional in action error/serviceunavailable

--- actions/error/serviceunavailable.php
+++ actions/error/serviceunavailable.php
@@ -2,19 +2,30 @@
 
 /**
  *
- * @copyright  2010-2012 izend.org
- * @version    1
+ * @copyright  2010-2016 izend.org
+ * @version    2
  * @link       http://www.izend.org
  */
 
-function serviceunavailable($lang, $arglist) {
+function serviceunavailable($lang, $arglist=false) {
 	head('title', translate('http_service_unavailable:title', $lang));
 	head('robots', 'noindex, nofollow');
 
 	$contact=false;
 	$banner = build('banner', $lang, compact('contact'));
 
-	list($closing_time, $opening_time) = $arglist;
+	$closing_time=true;
+	$opening_time=false;
+
+	if (is_array($arglist)) {
+		if (isset($arglist[0])) {
+			$closing_time=$arglist[0];
+		}
+		if (isset($arglist[1])) {
+			$opening_time=$arglist[1];
+		}
+	}
+
 	$content = view('error/serviceunavailable', $lang, compact('closing_time', 'opening_time'));
 
 	$output = layout('standard', compact('banner', 'content'));
