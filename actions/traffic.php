0ff8ce77a5de0ed5b0e7e715000d603fbb608f8b
izend <izend@izend.org>
2016-10-25 02:08:14 +0200

    Checks if configuration parameters are set in action traffic

--- actions/traffic.php
+++ actions/traffic.php
@@ -3,15 +3,21 @@
 /**
  *
  * @copyright  2016 izend.org
- * @version    1
+ * @version    2
  * @link       http://www.izend.org
  */
 
 require_once 'userhasrole.php';
 
 function traffic($lang) {
+	global $googleanalyticsaccount, $googleanalyticskeyfile;
+
 	if (!user_has_role('administrator')) {
 		return run('error/unauthorized', $lang);
+	}
+
+	if (! ($googleanalyticsaccount and $googleanalyticskeyfile)) {
+		return run('error/internalerror', $lang);
 	}
 
 	head('title', translate('traffic:title', $lang));
