a69881acb3b5b7dcf98560bae7a7707fcfe322ed
izend <izend@izend.org>
2016-10-30 19:27:30 +0100

    Removes requiring function dump in index.php

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 
 /**
  *
- * @copyright  2010-2012 izend.org
- * @version    3
+ * @copyright  2010-2016 izend.org
+ * @version    4
  * @link       http://www.izend.org
  */
 
@@ -12,8 +12,6 @@ define('ROOT_DIR', dirname(__FILE__));
 set_include_path(ROOT_DIR . PATH_SEPARATOR . get_include_path());
 set_include_path(ROOT_DIR . DIRECTORY_SEPARATOR . 'includes' . PATH_SEPARATOR . get_include_path());
 set_include_path(ROOT_DIR . DIRECTORY_SEPARATOR . 'library' . PATH_SEPARATOR . get_include_path());
-
-require_once 'dump.php';
 
 require_once 'bootstrap.php';
 
