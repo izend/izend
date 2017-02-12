d6bb793e185bd2a788b854d0b37658f090c7548c
izend <izend@izend.org>
2017-02-12 19:29:34 +0100

    Adds function dircopy

---
+++ library/dircopy.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ *
+ * @copyright  2017 izend.org
+ * @version    1
+ * @link       http://www.izend.org
+ */
+
+function dircopy($from, $to, $mode = 0777) {
+	if (!is_dir($from)) {
+		return false;
+	}
+
+	if (!is_dir($to)) {
+		if (!@mkdir($to)) {
+			return false;
+		}
+	}
+
+	dircopyaux($from, $to, $mode);
+
+	return true;
+}
+
+function dircopyaux($from, $to, $mode) {
+	$handle = opendir($from);
+	while (($file = readdir($handle)) !== false) {
+		if ($file == '.' || $file == '..') {
+			continue;
+		}
+		$frompath = $from . DIRECTORY_SEPARATOR . $file;
+		$topath = $to . DIRECTORY_SEPARATOR . $file;
+		if (is_link($frompath)) {
+			symlink(readlink($frompath), $topath);
+		}
+		else if (is_file($frompath)) {
+			copy($frompath, $topath);
+		}
+		else if (is_dir($frompath)) {
+			mkdir($topath, $mode);
+			dircopyaux($frompath, $topath, $mode);
+		}
+	}
+	closedir($handle);
+}
