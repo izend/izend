4403fec841e3d5b2554dfdbe66d955a7fdaa93db
izend <izend@izend.org>
2018-01-28 18:42:30 +0100

    Adds action confirmnewslettersubscribe

--- actions/confirmnewslettersubscribe.php
+++ actions/confirmnewslettersubscribe.php
@@ -2,14 +2,14 @@
 
 /**
  *
- * @copyright  2014-2018 izend.org
- * @version    4
+ * @copyright  2018 izend.org
+ * @version    1
  * @link       http://www.izend.org
  */
 
 require_once 'models/newsletter.inc';
 
-function confirmnewsletterunsubscribe($lang, $arglist) {
+function confirmnewslettersubscribe($lang, $arglist) {
 	head('title', translate('newsletter:title', $lang));
 	head('description', false);
 	head('keywords', false);
@@ -22,7 +22,7 @@ function confirmnewsletterunsubscribe($lang, $arglist) {
 	$bad_mail=false;
 	$bad_time=false;
 
-	if (!newsletter_get_user($mail)) {
+	if (newsletter_get_user($mail)) {
 		$bad_mail=true;
 	}
 
@@ -36,14 +36,13 @@ function confirmnewsletterunsubscribe($lang, $arglist) {
 	$contact_page=false;
 
 	if ($bad_mail) {
-		$subscribe_page=url('newslettersubscribe', $lang);
-	}
-	else if ($bad_time) {
 		$unsubscribe_page=url('newsletterunsubscribe', $lang);
 	}
-
+	else if ($bad_time) {
+		$subscribe_page=url('newslettersubscribe', $lang);
+	}
 	else {
-		$r = newsletter_delete_user($mail);
+		$r = newsletter_create_user($mail, $lang);
 
 		if (!$r) {
 			$internal_error=true;
@@ -56,11 +55,11 @@ function confirmnewsletterunsubscribe($lang, $arglist) {
 
 			$ip=server_ip_address();
 			$timestamp=strftime('%Y-%m-%d %H:%M:%S', time());
-			$subject = 'unsubscribe' . '@' . $sitename;
+			$subject = 'subscribe' . '@' . $sitename;
 			$msg = $ip . ' ' . $timestamp . ' ' . $lang . ' ' . $mail;
 			@emailme($subject, $msg);
 
-			$subscribe_page=url('newslettersubscribe', $lang);
+			$unsubscribe_page=url('newsletterunsubscribe', $lang);
 		}
 	}
 
@@ -70,7 +69,7 @@ function confirmnewsletterunsubscribe($lang, $arglist) {
 
 	$errors = compact('bad_mail', 'bad_time', 'internal_error', 'contact_page');
 
-	$content = view('confirmnewsletterunsubscribe', $lang, compact('mail', 'subscribe_page', 'unsubscribe_page', 'errors'));
+	$content = view('confirmnewslettersubscribe', $lang, compact('mail', 'subscribe_page', 'unsubscribe_page', 'errors'));
 
 	$output = layout('standard', compact('banner', 'content'));
 
