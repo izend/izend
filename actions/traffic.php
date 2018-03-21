bf212dc4b7e53e9d8fb4ab9adee0da322312895b
izend <izend@izend.org>
2018-03-21 23:00:53 +0100

    Deals with toolbar in traffic

--- actions/traffic.php
+++ actions/traffic.php
@@ -2,8 +2,8 @@
 
 /**
  *
- * @copyright  2016 izend.org
- * @version    3
+ * @copyright  2016-2018 izend.org
+ * @version    4
  * @link       http://www.izend.org
  */
 
@@ -11,6 +11,7 @@ require_once 'userhasrole.php';
 
 function traffic($lang) {
 	global $googleanalyticsaccount, $googleanalyticskeyfile;
+	global $with_toolbar;
 
 	if (!user_has_role('administrator')) {
 		return run('error/unauthorized', $lang);
@@ -20,16 +21,20 @@ function traffic($lang) {
 		return run('error/internalerror', $lang);
 	}
 
-	head('title', translate('traffic:title', $lang));
-
-	$admin=true;
-	$banner = build('banner', $lang, compact('admin'));
-
 	$analytics = build('analytics', $lang);
 
 	$content=view('traffic', $lang, compact('analytics'));
 
-	$output = layout('standard', compact('banner', 'content'));
+	head('title', translate('traffic:title', $lang));
+	head('description', false);
+	head('keywords', false);
+	head('robots', 'noindex');
+
+	$admin=true;
+	$banner = build('banner', $lang, $with_toolbar ? false : compact('admin'));
+	$toolbar = $with_toolbar ? build('toolbar', $lang, compact('admin')) : false;
+
+	$output = layout('standard', compact('toolbar', 'banner', 'content'));
 
 	return $output;
 }
