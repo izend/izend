75d347648065d03f6469b0b8ea82b213daafb680
izend <izend@izend.org>
2018-03-28 00:09:12 +0200

    Makes $action an optional argument of function nodeurl

--- library/nodeurl.php
+++ library/nodeurl.php
@@ -2,21 +2,15 @@
 
 /**
  *
- * @copyright  2010-2011 izend.org
- * @version    1
+ * @copyright  2010-2018 izend.org
+ * @version    2
  * @link       http://www.izend.org
  */
 
 require_once 'models/thread.inc';
 require_once 'models/node.inc';
 
-function nodeurl($lang, $thread_id, $node_id) {
-	global $db_debug;
-
-	$flag = $db_debug;
-	$db_debug = false;
-
-	$action = false;
+function nodeurl($lang, $thread_id, $node_id, $action=false) {
 	$args = array();
 
 	if ($thread_id) {
@@ -26,7 +20,9 @@ function nodeurl($lang, $thread_id, $node_id) {
 			return false;
 		}
 
-		$action = $r['thread_type'];
+		if (!$action) {
+			$action = $r['thread_type'];
+		}
 		$args[] = $r['thread_name'];
 	}
 
@@ -39,8 +35,6 @@ function nodeurl($lang, $thread_id, $node_id) {
 
 		$args[] = $r['node_name'];
 	}
-
-	$db_debug = $flag;
 
 	return url($action, $lang, $args);
 }
