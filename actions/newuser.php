48f47d93386479add991456ab38c3da61ed70262
izend <izend@izend.org>
2018-09-19 00:26:26 +0200

    Deals with toolbar in newuser

--- actions/newuser.php
+++ actions/newuser.php
@@ -3,23 +3,27 @@
 /**
  *
  * @copyright  2010-2018 izend.org
- * @version    2
+ * @version    3
  * @link       http://www.izend.org
  */
 
 function newuser($lang) {
+	global $with_toolbar;
+
+	$register = build('register', $lang);
+
 	head('title', translate('newuser:title', $lang));
 	head('description', false);
 	head('keywords', false);
 	head('robots', 'noindex');
 
-	$banner = build('banner', $lang);
-
-	$register = build('register', $lang);
+	$admin=true;
+	$banner = build('banner', $lang, $with_toolbar ? false : compact('admin'));
+	$toolbar = $with_toolbar ? build('toolbar', $lang, compact('admin')) : false;
 
 	$content = view('newuser', $lang, compact('register'));
 
-	$output = layout('standard', compact('banner', 'content'));
+	$output = layout('standard', compact('toolbar', 'banner', 'content'));
 
 	return $output;
 }
