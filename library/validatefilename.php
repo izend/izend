af3b8e7d7f4dfc174e220e3ba19e81c97b2f004b
izend <izend@izend.org>
2019-01-09 17:09:37 +0100

    Accepts a file name which starts with a digit in validate_filename

--- library/validatefilename.php
+++ library/validatefilename.php
@@ -2,12 +2,12 @@
 
 /**
  *
- * @copyright  2010-2018 izend.org
- * @version    3
+ * @copyright  2010-2019 izend.org
+ * @version    4
  * @link       http://www.izend.org
  */
 
 function validate_filename($name) {
-	return preg_match('/^[[:alpha:]]+[[:alnum:] \._-]*(\.[[:alnum:]]+)?$/', $name);
+	return preg_match('/^[[:alnum:]]+[[:alnum:] \._-]*(\.[[:alnum:]]+)?$/', $name);
 }
 
