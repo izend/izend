fe8c25bd2ff82ed0afc21220504f57150d53ea4e
izend <izend@izend.org>
2019-01-17 16:31:58 +0100

    Adds optional argument $hellip to function strtruncate

--- library/strtruncate.php
+++ library/strtruncate.php
@@ -2,11 +2,11 @@
 
 /**
  *
- * @copyright  2018 izend.org
- * @version    1
+ * @copyright  2018-2019 izend.org
+ * @version    2
  * @link       http://www.izend.org
  */
 
-function strtruncate($s, $len, $atword=false) {
-	return strlen($s) > $len ? ($atword ? preg_replace("/^(.{1,$len})(\s.*|$)/s", '\\1...', $s) : substr($s, 0, $len) . '...') : $s;
+function strtruncate($s, $len, $hellip='...', $atword=false) {
+	return strlen($s) > $len ? ($atword ? preg_replace("/^(.{1,$len})(\s.*|$)/s", '\\1' . $hellip, $s) : substr($s, 0, $len) . $hellip) : $s;
 }
