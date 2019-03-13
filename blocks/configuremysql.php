1673e3d0b5834cb1bb0ba72bf3493a2d5bda6de6
izend <izend@izend.org>
2019-03-14 00:05:27 +0100

    Edits link to iZend documentation in configuremysql and configurepgsql

--- blocks/configuremysql.php
+++ blocks/configuremysql.php
@@ -3,7 +3,7 @@
 /**
  *
  * @copyright  2014-2019 izend.org
- * @version    10
+ * @version    11
  * @link       http://www.izend.org
  */
 
@@ -462,8 +462,8 @@ _SEP_;
 INSERT INTO `${db_prefix}content_text` (`content_id`, `locale`, `text`, `eval`) VALUES
 (1, 'fr', '<p>Votre site <b>iZend</b> est maintenant opérationnel.</p>\r\n<p class="readmore">Lisez la <a href="/fr/documentation">documentation</a>.</p>\r\n<p>Validé avec\r\n<span class="btn_browser" id="browser_firefox" title="Firefox">Firefox</span>,\r\n<span class="btn_browser" id="browser_chrome" title="Chrome">Chrome</span>,\r\n<span class="btn_browser" id="browser_safari" title="Safari">Safari</span>,\r\n<span class="btn_browser" id="browser_opera" title="Opera">Opera</span>\r\net\r\n<span class="nowrap"><span class="btn_browser" id="browser_ie" title="Internet Explorer">Internet Explorer</span>.</span></p>', '0'),
 (1, 'en', '<p>Your <b>iZend</b> site is now operational.</p>\r\n<p class="readmore">Read the <a href="/en/documentation">documentation</a>.</p>\r\n<p>Validated with <span class="btn_browser" id="browser_firefox" title="Firefox">Firefox</span>,\r\n<span class="btn_browser" id="browser_chrome" title="Chrome">Chrome</span>,\r\n<span class="btn_browser" id="browser_safari" title="Safari">Safari</span>,\r\n<span class="btn_browser" id="browser_opera" title="Opera">Opera</span>\r\nand\r\n<span class="nowrap"><span class="btn_browser" id="browser_ie" title="Internet Explorer">Internet Explorer</span>.</span></p>', '0'),
-(2, 'fr', '<p class="readmore">Consultez la <a href="http://www.izend.org/fr/documentation">documentation en ligne</a>.</p>', '0'),
-(2, 'en', '<p class="readmore">Read the <a href="http://www.izend.org/en/documentation">on-line documentation</a>.</p>', '0');
+(2, 'fr', '<p class="readmore">Consultez la <a href="http://www.izend.org">documentation en ligne</a>.</p>', '0'),
+(2, 'en', '<p class="readmore">Read the <a href="http://www.izend.org">on-line documentation</a>.</p>', '0');
 _SEP_;
 		$db_conn->exec($sql);
 
