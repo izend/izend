b5f5227394ef3f129b8568ab4388392cf3875f70
izend <izend@izend.org>
2021-08-01 12:24:29 +0200

    Adds lower case letters to newpassword

--- library/newpassword.php
+++ library/newpassword.php
@@ -2,15 +2,15 @@
 
 /**
  *
- * @copyright  2010-2011 izend.org
- * @version    1
+ * @copyright  2010-2021 izend.org
+ * @version    2
  * @link       http://www.izend.org
  */
 
 require_once 'strrand.php';
 
-function newpassword($len=6) {
-	$charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+function newpassword($len=8) {
+	$charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
 	return strrand($charset, $len);
 }
