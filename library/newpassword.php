d43b63f98b8c948f8d3827a9ae9cd5e8a119bcbb
izend <izend@izend.org>
2022-04-27 18:33:57 +0200

    Makes sure function newpassword returns a minimum of 4 characters with at least an uppercase and a lowercase letter and a digit

--- library/newpassword.php
+++ library/newpassword.php
@@ -2,16 +2,24 @@
 
 /**
  *
- * @copyright  2010-2021 izend.org
- * @version    2
+ * @copyright  2010-2022 izend.org
+ * @version    3
  * @link       http://www.izend.org
  */
 
 require_once 'strrand.php';
 
 function newpassword($len=8) {
-	$charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	$charset = array('ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz', '0123456789');
 
-	return strrand($charset, $len);
+	$pwd = strrand($charset[0], 1) . strrand($charset[1], 1) . strrand($charset[2], 1);
+
+	$len = max($len, 4) - strlen($pwd);
+
+	while ($len-- > 0) {
+		$pwd .= strrand($charset[rand(0, count($charset) - 1)], 1);
+	}
+
+	return str_shuffle($pwd);
 }
 
