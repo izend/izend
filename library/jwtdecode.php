b9147d78c93573b26d085e39bacf9db84e5ec858
izend <izend@izend.org>
2023-06-19 15:35:39 +0200

    Edits jwtdecode.php

--- library/jwtdecode.php
+++ library/jwtdecode.php
@@ -2,12 +2,12 @@
 
 /**
  *
- * @copyright  2022 izend.org
- * @version    1
+ * @copyright  2022-2023 izend.org
+ * @version    2
  * @link       http://www.izend.org
  */
 
 function jwtdecode($token) {
-	return json_decode(base64_decode(str_replace('_', '/', str_replace('-','+',explode('.', $token)[1]))));
+	return json_decode(base64_decode(str_replace('_', '/', str_replace('-','+', explode('.', $token)[1]))));
 }
 
