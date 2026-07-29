e18cbd4210bd06fa67d1b7fab049333e11cfdd1f
izend <izend@izend.org>
2026-07-29 17:42:19 +0200

    Always includes and updates OpenGraph tags in head.phtml

--- library/head.php
+++ library/head.php
@@ -2,8 +2,8 @@
 
 /**
  *
- * @copyright  2010-2017 izend.org
- * @version    13
+ * @copyright  2010-2026 izend.org
+ * @version    14
  * @link       http://www.izend.org
  */
 
@@ -36,6 +36,9 @@ function head($type=false) {
 			break;
 		case 'image':
 			$head['image'] = $args[0];
+			break;
+		case 'image_alt':
+			$head['image_alt'] = $args[0];
 			break;
 		case 'canonical':
 			$head['canonical'] = $args[0];
