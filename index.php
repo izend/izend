0b7dca7da0d8a63cb608a5770e09afe4ee883e2b
izend <izend@izend.org>
2026-07-31 16:25:24 +0200

    Replaces dirname(__FILE__) by __DIR__ in index.php

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 
 /**
  *
- * @copyright  2010-2016 izend.org
- * @version    4
+ * @copyright  2010-2026 izend.org
+ * @version    5
  * @link       http://www.izend.org
  */
 
-define('ROOT_DIR', dirname(__FILE__));
+define('ROOT_DIR', __DIR__);
 
 set_include_path(ROOT_DIR . PATH_SEPARATOR . get_include_path());
 set_include_path(ROOT_DIR . DIRECTORY_SEPARATOR . 'includes' . PATH_SEPARATOR . get_include_path());
