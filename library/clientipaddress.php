7db0d985014623320e13642e6202d33c4a94ef57
izend <izend@izend.org>
2026-08-02 19:09:51 +0200

    Replaces php_sapi_name() by PHP_SAPI in client_ip_address in library

--- library/clientipaddress.php
+++ library/clientipaddress.php
@@ -2,12 +2,12 @@
 
 /**
  *
- * @copyright  2010-2014 izend.org
- * @version    5
+ * @copyright  2010-2026 izend.org
+ * @version    6
  * @link       http://www.izend.org
  */
 
 function client_ip_address() {
-	return isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : (php_sapi_name() == 'cli' ? '127.0.0.1' : false);	// DON'T TRUST $_SERVER['HTTP_X_FORWARDED_FOR']
+	return isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : (PHP_SAPI == 'cli' ? '127.0.0.1' : false);	// DON'T TRUST $_SERVER['HTTP_X_FORWARDED_FOR']
 }
 
